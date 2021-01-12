1a65327e5106bebab1412f456e304647e0214762
Andrew Aikman <andrew.aikman@eliga.services>
2021-01-12 15:18:58 +0000

    Ensure the setup.py fiel is ready for deployment

--- setup.py
+++ setup.py
@@ -6,8 +6,16 @@ from djangocms_4_migration import __version__
 setup(
     name='djangocms-4-migration',
     version=__version__,
-    description=open('README.md').read(),
+    author="Andrew Aikman (Aiky30)",
+    description="A django CMS 3 to 4 migration package",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
     include_package_data=True,
     zip_safe=False,
 )
